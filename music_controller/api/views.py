b046b293a180397ec5288d784879cf3a43f13ba5
Henry <heinrichk91@gmail.com>
2025-11-03 15:07:27 +0200

    join room

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -6,9 +6,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerialiser
+
+class GetRoom(APIView):
+    serializer_class = RoomSerialiser
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwarg)
+        if code is not None:
+            room =Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerialiser(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room not found': 'Invalid Room Code'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Code param not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -27,9 +42,32 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(updates_fields=['guest_can_pause', 'votes_to_skip'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerialiser(room).data, status=status.HTTP_201_CREATED)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
-        
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerialiser(room).data, status=status.HTTP_201_CREATED)
+
         return Response(RoomSerialiser(room).data, status=status.HTTP_201_CREATED)
+
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        code = request.data.get(self.lookup_url_kwarg)
+
+        if code is not None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': 'Invalid post data, did not find a code Key'}, status=status.HTTP_400_BAD_REQUEST)
